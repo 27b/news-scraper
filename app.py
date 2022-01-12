509532393c8bd8effb669a8d01b78b972dcf6d64
27b <baldassani@tuta.io>
2022-01-12 09:51:20 -0300

    Adding .env and PostView

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from views import (IndexView, PostView)
+from views import IndexView, PostView
 from os import getenv
 from dotenv import load_dotenv
 
+
 # Flask configuration
+load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = getenv('TRACK_MODIFICATIONS')
-app.config['DEBUG'] = getenv('DEBUG_MODE')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = eval(getenv('TRACK_MODIFICATIONS'))
+app.config['DEBUG'] = eval(getenv('DEBUG_MODE'))
 
 db = SQLAlchemy(app=app)
 migrate = Migrate(app=app, db=db)
@@ -18,6 +20,7 @@ migrate = Migrate(app=app, db=db)
 
 # Flask urls
 app.add_url_rule('/', view_func=IndexView.as_view('index'))
+app.add_url_rule('/posts/<int:post_id>', view_func=PostView.as_view('posts'))
 
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,14 +1,19 @@
-from flask import render_template
+from flask import render_template, jsonify
 from flask.views import MethodView
 
 
 class IndexView(MethodView):
+    """
+    On init, is rendered with Jinja, on update get data from Api using
+    JavaScript and SocketIO.
+    """
 
     def get(self):
         return render_template('index.html')
 
 
 class PostView(MethodView):
+    """Returns post data by id."""
 
-    def get(self, post_id):
-        return {'name': 'Hello World'}
+    def get(self, post_id: int):
+        return {'id': post_id}
