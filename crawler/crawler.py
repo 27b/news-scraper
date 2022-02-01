0c018bf654259cb35fe0fd9f3cf019853629d32a
27b <baldassani@tuta.io>
2022-02-01 16:30:08 -0300

    Updating: Crawler, clean dead code of __insert_in_database

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -27,38 +27,38 @@ class Crawler:
             Run permanently.
         """
         if not cls.__newsletters:
-            cls.__newsletters = [M for M in Newsletter.query.all()]
+            cls.__newsletters = [N for N in Newsletter.query.all()]
             print('Newsletter list created:', cls.__newsletters)
         if not cls.__categories:
-            cls.__categories = [M for M in Category.query.all()]
+            cls.__categories = [C for C in Category.query.all()]
             print('Categories list created:', cls.__categories)
         if cls.__database:
             db = cls.__database
             for post in list_of_posts:
+                title = post.get('title')
                 category = post.get('category')
                 newsletter_in_db = list(filter(lambda x: x.name == name, cls.__newsletters))
                 category_in_db = list(filter(lambda x: x.name == category, cls.__categories))
-                post_in_db = Post.query.filter_by(title=post.get('title')).first()
-                try:
-                    if category_in_db == []:
-                        print('ERROR: Category does not exist.')
-                    elif newsletter_in_db[0] and category_in_db[0]:
-                        if not post_in_db:
-                            new_post = Post()
-                            new_post.title = post.get('title')
-                            new_post.description = post.get('description')
-                            new_post.author = post.get('author')
-                            new_post.url = post.get('url')
-                            new_post.newsletter_id = newsletter_in_db[0].id
-                            new_post.category_id = category_in_db[0].id
-                            db.session.add(new_post)
-                            db.session.commit()
-                        else:
-                            pass  # Post already in database
+                if newsletter_in_db == []:
+                    print('ERROR: Newsletter does not exist.')
+                elif category_in_db == []:
+                    print('ERROR: Category does not exist.')
+                elif newsletter_in_db[0] and category_in_db[0]:
+                    post_in_db = Post.query.filter_by(title=title).first()
+                    if not post_in_db:
+                        new_post = Post()
+                        new_post.title = post.get('title')
+                        new_post.description = post.get('description')
+                        new_post.author = post.get('author')
+                        new_post.url = post.get('url')
+                        new_post.newsletter_id = newsletter_in_db[0].id
+                        new_post.category_id = category_in_db[0].id
+                        db.session.add(new_post)
+                        db.session.commit()
                     else:
-                        print(f'ERROR: Newsletter {name} not in database.')
-                except Exception as e:
-                    print(f'ERROR: {e}')
+                        pass  # Post already in database
+                else:
+                    pass  # There is no other case
         else:
             print('ERROR: Database not instanced.')
 
@@ -68,7 +68,6 @@ class Crawler:
         scrapers and send data in cls, this data is inserted in the database.
         """
         cls.__database = database
-
         while True:
             sleep(TIME_FOR_SLEEP)
             for newsletter in SCRAPER_LIST:
