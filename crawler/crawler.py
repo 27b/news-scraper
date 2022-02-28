f4d43f69c6800e3ac55b77e0be1ecd242e1e8888
27b <baldassani@tuta.io>
2022-02-28 19:06:19 -0300

    Refactoring: Crawler (2/4): Development of all classes

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from crawler.config import list_of_sites
 from crawler.core.scraper import AutoScraperScraper
 from crawler.core.spider import BasicSpider
@@ -10,7 +11,13 @@ TIME_FOR_SLEEP = 60 * 10
 SCRAPER_LIST = list_of_sites
 
 
-class CrawlerSchema:
+class __CrawlerSchema:
+    '''CrawlerSchema is the handler of the database.
+    
+    Args:
+        app: An instance of Flask application.
+        database: An instance of SQLAlchemy database.
+    '''
     __instance = None
 
     def __init__(self, app, db):
@@ -23,66 +30,155 @@ class CrawlerSchema:
             cls.__instance = instance
         return cls.__instance
 
-    def __check_database_status(self) -> bool:
+    def check_database_status(self) -> bool:
         '''Checks if the database and the application were instantiated,
         it also checks if the newsletter exists in the database.'''
         if not self.__app:
             print('The application has not been instantiated.')
             return False
-        
+
         if not self.__db:
             print('The database has not been instantiated.')
             return False
-    
+
         return True
 
-    def __get_categories(self) -> list[Category]:
+    def get_categories(self) -> list[Category]:
         '''Returns a dictionary with all existing categories.'''
         with self.__app.app_context():
             query = Category.query.all() 
             return [C for C in query]
 
-    def __get_newsletters(self) -> list[Newsletter]:
-        '''Returns a dictionary with all existing newsletters'''
+    def get_newsletters(self) -> list[Newsletter]:
+        '''Returns a dictionary with all existing newsletters.'''
         with self.__app.app_context():
             query = Newsletter.query.all()
             return [N for N in query]
 
-    def __save_post_in_database(self, new_post) -> bool:
+    def save_post_in_database(self, new_post: Post) -> bool:
+        '''Save post in database using app context.
+        
+        Args:
+            new_post: Post type.
+
+        Returns:
+            True: If the post was saved in the database.
+            False: If an error occurred.
+        '''
         try:
-            self.__db.session.add(new_post)
-            self.__db.session.commit()
+            if self.__check_database_status():
+                with self.__app.app_context():
+                    self.__db.session.add(new_post)
+                    self.__db.session.commit()
+            else:
+                return False
+
         except Exception as error:
-            print(error)
+            print('SAVE POST ERROR:', error)
             return False
         else:
             return True
 
 
-class CrawlerMediator:
-    '''Get data of crawler and apply logic to save data using crawler schema'''
-    schema = CrawlerSchema()
+class CrawlerDataHandler:
+    '''Get data of crawler and apply logic to save data using crawler schema.
+    
+    Args:
+        app: An instance of Flask application.
+        database: An instance of SQLAlchemy database.
+    '''
 
-    @classmethod
-    def save_post(cls, new_post) -> bool:
-        if cls.schema.__check_if_post_is_valid():
+    def __init__(self, app, db) -> None:
+        self.schema = __CrawlerSchema(app, db)
+        self.newsletters = [N.name for N in self.schema.get_newsletters()]
+        self.categories = [C.name for C in self.schema.get_categories()]
+
+    def save_post(cls, new_post: Post) -> bool | None:
+        '''Save Post in database using CrawlerSchema if the post is valid.
+
+        Args:
+            Post: Post type.
+
+        Returns:
+            bool: True or false depending on the result of the database. 
+        '''
+        if cls.schema.__check_if_post_is_valid(new_post):
             return cls.schema.__save_post_in_database(new_post)
     
-    @classmethod
-    def check_if_post_is_valid(cls, new_post) -> bool:
-        pass
+    def check_if_post_is_valid(self, post: dict) -> Post | bool:
+        '''Check if the newsltetter and category of the post is valid.
+        
+        Args:
+            post: Is a dict with: title, description, author, url, category
+                  and Newsletter.
+
+        Returns:
+            Post: if the post is valid.
+            bool: False if category or newsletter is invalid.
+        '''
+        if not post.category in self.categories: return False
+        if not post.newsletter in self.newsletters: return False
+        
+        # Next: Change database query for lambda
+        newsletter_query = Newsletter.query.filter_by(name=post.newsletter)
+        category_query = Category.query.filter_by(name=post.category)
+        
+        new_post = Post()
+        new_post.title = post.get('title')
+        new_post.description = post.get('description')
+        new_post.author = post.get('author')
+        new_post.url = post.get('url')
+        new_post.newsletter_id = newsletter_query.first()
+        new_post.category_id = category_query.first()
+
+        return new_post
 
 
-class CrawlerManager:
-    '''Execute Crawler and send data to mediator,'''
+class Crawler2:
     
     @classmethod
-    def execute_crawler():
-        pass
+    def run_task(cls, app, db) -> None:
+        '''This method execute the scrapers and send data in cls, this data
+        is inserted in the database.
+
+        Args:
+            app: An instance of Flask application.
+            db: An instance of SQLAlchemy database.
+        
+        Returns:
+            None - Run permanently.
+        '''
+        cls.__app = app
+        cls.__db = db
+
+        while True:
+            for newsletter in SCRAPER_LIST:
+                print(f"* NEWSLETTER: {newsletter['name']}")
+                spider = BasicSpider(AutoScraperScraper())
+                spider.execute_scraper(newsletter)
+                result = spider.result()
+                if result: cls.send_data_to_handler(newsletter['name'], result)
+            sleep(TIME_FOR_SLEEP)
 
     @classmethod
-    def send_data_to_mediator():
-        pass
+    def send_data_to_handler(cls, newsletter: str, post_list: list[dict]):
+        '''Send data to Data Handler and check if the post is valid, if is
+        valid save the data.
+        
+        Args:
+            newsletter: The name in lowercase of the newsletter.
+            post_list: A list of dictionaries with the attributes of Post.
+        
+        Returns:
+            None: You can extend this if you want add logs of the state of
+            posts.
+        '''
+        data_handler = CrawlerDataHandler(cls.__app, cls.__db)
+        for post in post_list:
+            post['newsletter'] = newsletter
+            new_post = data_handler.check_if_post_is_valid(post)
+            if new_post:
+                data_handler.save_post(new_post)
 
 
 class Crawler:
